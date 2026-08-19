118c397295ccb597f4195e71eff84e8cc9cfb081
ilhamsadath29 <ilhamsadath29@gmail.com>
2026-08-19 11:53:07 +0530

    feat: register model observers and configure Vite and HTTPS in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // if (config('app.env') === 'production' || config('app.env') === 'prod') {
-        //     URL::forceScheme('https');
-        // }
+        if (config('app.env') === 'production' || config('app.env') === 'prod') {
+            URL::forceScheme('https');
+        }
 
         Vite::prefetch(concurrency: 3);
         \App\Models\Accounting\JournalEntryLine::observe(\App\Observers\JournalEntryLineObserver::class);
