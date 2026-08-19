f29b6cd743f63b4b1bb282187cd0de8b1bb17f94
ilhamsadath29 <ilhamsadath29@gmail.com>
2026-08-19 11:58:12 +0530

    feat: register model observers and configure Vite prefetching in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,10 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production' || config('app.env') === 'prod') {
-            URL::forceScheme('https');
-        }
-
         Vite::prefetch(concurrency: 3);
         \App\Models\Accounting\JournalEntryLine::observe(\App\Observers\JournalEntryLineObserver::class);
         \App\Models\Accounting\JournalEntry::observe(\App\Observers\JournalEntryObserver::class);
