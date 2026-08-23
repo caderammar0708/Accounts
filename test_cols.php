289217e04effb44f5092c5a067078ced69f7ccdb
ilhamsadath29 <ilhamsadath29@gmail.com>
2026-08-23 15:21:39 +0530

    chore: remove obsolete development test scripts

--- test_cols.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-echo "Tanks: " . json_encode(Illuminate\Support\Facades\Schema::getColumnListing('tanks')) . "\n";
-echo "TankDipReadings: " . json_encode(Illuminate\Support\Facades\Schema::getColumnListing('tank_dip_readings')) . "\n";
